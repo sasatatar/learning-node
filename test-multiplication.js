ed50dc596199c1b544872cf19c2bc48ff615e16c
Sasha <sasa.tatar@gmail.com>
2020-02-02 01:16:38 +0100

    ...

--- test-multiplication.js
+++ test-multiplication.js
@@ -4,39 +4,44 @@ const rl = readline.createInterface({
     output: process.stdout
 });
 
-let score = 0;
-let question = 0;
+async function startNewTest() {
+    let score = 0;
+    let question = 0;
 
-function askAnotherQuestion() {
-    question += 1;
-    let num1 = Math.floor(Math.random() * 10 + 1);
-    let num2 = Math.floor(Math.random() * 10 + 1);
-    let result = num1 * num2;
+    while (question < 10) {
+        question++;
+        let resp = await askAnotherQuestion(question);
+        if (resp) 
+            score++;
+    }
     
-    rl.question(`${question}. What is ${num1} * ${num2}?\n`, (input) => {
-        let answer = Number(input.trim());
-        if (answer === result) {
-           console.log("Correct!\n");
-           score++;
+    console.log(`Your score is: ${score}/${question}\n`);
+    rl.question("Play again? (y/n)\n", res => {
+        if (res === 'y') {
+            startNewTest();
         } else {
-            console.log("Incorrect!\n");
+            rl.close();
         }
-        if (question < 10)
-            askAnotherQuestion();
-        else {
-            console.log(`Your score is: ${score}/${question}\n`);
-            rl.question("Play again? (y/n)\n", res => {
-                if (res === 'y') {
-                    score = 0;
-                    question = 0;
-                    console.log('\n');
-                    askAnotherQuestion();
-                } else {
-                    rl.close();
-                }
-            })
-        }
+    })
+}
+
+function askAnotherQuestion(question) {
+    return new Promise(resolve => {
+        let num1 = Math.floor(Math.random() * 10 + 1);
+        let num2 = Math.floor(Math.random() * 10 + 1);
+        let result = num1 * num2;
+        
+        rl.question(`${question}. What is ${num1} * ${num2}?\n`, (input) => {
+            let answer = Number(input.trim());
+            if (answer === result) {
+                console.log("Correct!\n");
+                resolve(true);
+            } else {
+                console.log("Incorrect!\n");
+                resolve(false);
+            }
+        });
     });
 }
 
-askAnotherQuestion();
+startNewTest();
